7b6a46580d23edfedda365218fd6c2e2f468bc2e
unknown <lazycode686@gmail.com>
2023-02-22 08:02:19 -0800

    version 1.0

---
+++ js/script.js
@@ -0,0 +1,96 @@
+// ==== NAV MENU ==== //
+const navId = document.getElementById("nav_menu"),
+  ToggleBtnId = document.getElementById("toggle_btn"),
+  CloseBtnId = document.getElementById("close_btn");
+
+// ==== SHOW MENU ==== //
+ToggleBtnId.addEventListener("click", () => {
+  navId.classList.add("show");
+});
+
+// ==== HIDE MENU ==== //
+CloseBtnId.addEventListener("click", () => {
+  navId.classList.remove("show");
+});
+
+// ==== ONCLICK PLAY ADIO AND PAUSE ==== //
+const audio = document.getElementById("audio");
+audio.volume = 0.5;
+
+// audio.play();
+// ==== PLAY MUSIC ==== //
+const playBtn = document.getElementById("play-btn"),
+  pauseBtn = document.getElementById("pause-btn");
+playBtn.addEventListener("click", () => {
+  audio.play();
+  playBtn.classList.remove("hide");
+  pauseBtn.classList.add("hide");
+});
+// ==== PAUSE MUSIC ==== //
+pauseBtn.addEventListener("click", () => {
+  audio.pause();
+  pauseBtn.classList.remove("hide");
+  playBtn.classList.add("hide");
+});
+
+// ==== COSTUMER REVIEW SLIDER ==== //
+
+
+
+
+
+// ==== Animate on Scroll Initialize  ==== //
+AOS.init();
+
+
+// ==== GSAP Animations ==== //
+// ==== LOGO  ==== //
+gsap.from(".logo", {
+  opacity: 0,
+  y: -10,
+  delay: 1,
+  duration: 0.5,
+});
+// ==== NAV-MENU ==== //
+gsap.from(".nav_menu_list .nav_menu_item", {
+  opacity: 0,
+  y: -10,
+  delay: 1.4,
+  duration: 0.5,
+  stagger: 0.3,
+});
+// ==== TOGGLE BTN ==== //
+gsap.from(".toggle_btn", {
+  opacity: 0,
+  y: -10,
+  delay: 1.4,
+  duration: 0.5,
+});
+// ==== MAIN HEADING  ==== //
+gsap.from(".main-heading", {
+  opacity: 0,
+  y: 20,
+  delay: 2.4,
+  duration: 1,
+});
+// ==== INFO TEXT ==== //
+gsap.from(".info-text", {
+  opacity: 0,
+  y: 20,
+  delay: 2.8,
+  duration: 1,
+});
+// ==== CTA BUTTONS ==== //
+gsap.from(".btn_wrapper", {
+  opacity: 0,
+  y: 20,
+  delay: 2.8,
+  duration: 1,
+});
+// ==== TEAM IMAGE ==== //
+gsap.from(".team_img_wrapper img", {
+  opacity: 0,
+  y: 20,
+  delay: 3,
+  duration: 1,
+});
